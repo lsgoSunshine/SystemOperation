02f6d948a12d6f2fa7cec1e87ec2f0e556ca49ea
Dicoee <9958769244@qq.com>
2016-11-09 13:56:48 +0800

    success

--- HRRN/list.c
+++ HRRN/list.c
@@ -7,7 +7,7 @@
 typedef struct PCB  
 {  
     int ID;                         //标识符 
-	char* name;                     //名字 
+	char name;                     //名字 
     double Level;                   //优先级  
     int ComeTime;                   //到达时间  
     int ServerTime;                 //服务时间  
@@ -27,8 +27,9 @@ typedef struct Node *List;
 
 
 //添加PCB 
-PCB * InsertPCB(char* name,int ComeTime,int ServerTime){
+PCB * InsertPCB(char name,int ComeTime,int ServerTime){
 	PCB *p=(PCB*)malloc(sizeof(PCB));
+	//p->name=name;
 	p->name=name;
 	p->ComeTime=ComeTime;
 	p->ServerTime=ServerTime;
@@ -37,13 +38,13 @@ PCB * InsertPCB(char* name,int ComeTime,int ServerTime){
 
 
 //添加节点 
-void InsertNode(PCB *p,List *list){
-	Node *current=*list;
+void InsertNode(PCB *p,List *PList){
+	Node *current=*PList;
 	Node *temp=(Node*)malloc(sizeof(Node));
 	temp->data=p;
 	temp->next=NULL;
-	if(*list==NULL){
-		*list=temp;
+	if(*PList==NULL){
+		*PList=temp;
 	}else{
 		while(current->next!=NULL){
 			current=current->next;
@@ -53,21 +54,20 @@ void InsertNode(PCB *p,List *list){
 		
 } 
 //删除节点 
-bool DelectNode(List *list,Node *node){
-	Node *current=*list;
-	if(strcmp(current->data->name,node->data->name)==0){
-		*list=node->next;
+bool DelectNode(List * PList,Node *node){
+	Node *current = *PList;
+	if(current->data->name==node->data->name){
+		*PList=node->next;
 		free(node);
 		return true; 
 	}
 	while(current->next!=NULL){
-		if(current->next==node){
-			current=node->next;
+		
+		if(current->next->data->name==node->data->name){
+			current->next=node->next;
 			free(node);
 			return true;	
-		}else{
-			current=current->next;
 		}
-		return false;
+		free(node);
 	}
 }
